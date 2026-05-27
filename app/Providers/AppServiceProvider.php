4101e9ba00c85d9da538213c857a6a0ded5cfa69
Алина Шакова <alinasakova@Air-Alina.IGD_Rostelecom>
2026-05-27 15:23:49 +0500

    Fix: добавлен отсутствующий импорт фасада URL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <-- Вот эта строка решает проблему
 
 class AppServiceProvider extends ServiceProvider
 {
